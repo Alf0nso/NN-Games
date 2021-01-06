c941eef84fd7e07f421175b4e185d9b689cc9fbf
Afonso <afonsorafael@sapo.pt>
2021-01-06 15:23:49 +0000

    Demo of NN
    changed the demo to new file

---
+++ nn_sum_demo.py
@@ -0,0 +1,26 @@
+# Demo demonstrating neural network implementation
+# learning how to sum two numbers, (exciting I know)
+#
+
+import neural_net as nn
+import numpy as np
+from random import random
+
+
+mlp = nn.MLP(2, [5], 1)
+
+#  [weights, activations, derivatives]
+inputs = np.array([[random() /
+                    2 for _ in range(2)]
+                   for _ in range(3000)])
+
+target = np.array([[i[0] + i[1]] for i in inputs])
+
+nn.train(mlp, inputs, target, 50, 1)
+
+input = np.array([0.3, 0.2])
+
+output = nn.foward_propagate(input, mlp[0], mlp[1], mlp[2])
+print()
+print("Prediction of {} + {} is {}".format(
+    input[0], input[1], output[0]))
