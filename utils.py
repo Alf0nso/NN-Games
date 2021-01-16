8c01b47d0d1f6a651567ba14b4196c3949539dd1
Afonso <afonsorafael@sapo.pt>
2021-01-16 18:44:05 +0000

    utils commented
    Wrote all the function definitions.
    nn_construct_input should be changed
    so it's not so bloated.

--- utils.py
+++ utils.py
@@ -11,23 +11,23 @@ import os
 import numpy as np
 from itertools import chain
 
-# Only the os lib is used mainly to clear
+# The os lib is used mainly to clear
 # the screen of the terminal or to write files
-# in memory
+# in memory. Numpy is used because of it's
+# math processing power.
 
 
 def clear():
     """
     Simply clears any terminal screen.
     Only meant to make things more readable.
-    Should be moved into a separate file
-    called "utils"
     """
     if os.name == 'nt':
         _ = os.system('cls')
 
     else:
         _ = os.system('clear')
+    pass
 
 
 def build_board(r, c, f=" ", t="s"):
@@ -35,10 +35,23 @@ def build_board(r, c, f=" ", t="s"):
     Any board that can be represented on a
     tabular manner can be created with this
     function.
+    r, c stand for row and column
+    f is what we fill in the board with
+    t is the type of the thing we use to fill
+    the board
     """
+
+    # If type is i then we get an integer
     if t == "i":
         return [[int(f)] * c
                 for i in range(r)]
+
+    # If type is f then we get a float
+    if t == "f":
+        return [[float(f)] * c
+                for i in range(r)]
+
+    # Any other type we will get a string
     return [[f] * c for i in range(r)]
 
 
@@ -58,7 +71,7 @@ def print_board(board):
             print(cell, end="|")
         print()
         print("+-" * len(row) + "+")
-    return(0)
+    return 0
 
 
 def check_board(board, char=" ") -> bool:
@@ -66,7 +79,6 @@ def check_board(board, char=" ") -> bool:
     check if certain character
     is on the board!
     """
-
     for row in board:
         if char in row:
             return True
@@ -74,6 +86,11 @@ def check_board(board, char=" ") -> bool:
 
 
 def nn_construct_input(file, r, c, symbol1='X', symbol2='O', n=0):
+    """
+    Builds input that is goind to be used to train the
+    neural network. it's not the best function, there
+    is probably a better way to do this.
+    """
     games = open(file, "rt")
 
     targets = []
