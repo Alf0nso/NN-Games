196a5850b48e0d4d3bdb5a10afa6e977eb3b725b
Afonso <afonsorafael@sapo.pt>
2021-01-17 16:50:23 +0000

    addition of prints and comments
    Changed the Neural Network Demo sum, and added some
    comments and prints for better readability.

--- nn_sum_demo.py
+++ nn_sum_demo.py
@@ -1,12 +1,19 @@
-# Demo demonstrating neural network implementation
-# learning how to sum two numbers, (exciting I know)
+# Neural Network Demo
 #
+# @Author: Afonso Rafael & Renata
+#
+# Demo demonstrating neural network implementation
+# learning how to sum two numbers, (exciting I know).
+# Can be used to understand how can be used and
+# altered to be able to map other functions.
 
 import neural_net as nn
 import numpy as np
 from random import random
 
+# Libraries necessary to perform the task.
 
+##################################################
 mlp = nn.MLP(2, [5], 1)
 
 #  [weights, activations, derivatives]
@@ -15,12 +22,18 @@ inputs = np.array([[random() /
                    for _ in range(3000)])
 
 target = np.array([[i[0] + i[1]] for i in inputs])
-print(target)
-print(inputs)
-input()
 
+##################################################
+print(50*"-")
+print("Training The Neural Network!")
+print()
 nn.train(mlp, inputs, target, 50, 1)
 
+
+print()
+print(50*"-")
+print("Testing The Neural Network!")
+print()
 input = np.array([0.3, 0.2])
 
 output = nn.forward_propagate(input, mlp[0], mlp[1])
