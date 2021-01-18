65e23a1b3ce27af8dd17d323191245867c378812
Afonso <afonsorafael@sapo.pt>
2021-01-18 19:37:37 +0000

    Main is now a proper showcase
    In the future that main file should be changed
    so we are able to change some options, including
    the neural networks used, etc...
    for now, it's just a show case of the work accomplished
    until now!

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+# Show case
+#
+# @Author: Afonso Rafael & Renata
+#
+# Show case of the neural networks that
+# where trained on the presented data.
+# It's not a very versatile "main" file... it's
+# literally just a showcase the work done
+# until now.
+
 import utils as ut
-import numpy as np
 import connect4 as c4
 import tic_tac_toe as tic
-import games_generator as gg
+
+# Libraries used are the libraries
+# that where developed by us.
 
 loop = True
 
@@ -13,8 +24,12 @@ print(" " + 40*"-")
 print("\n\n")
 print("1. To play Tic Tac Toe (TTT)")
 print("2. To play Connect Four (C4)")
+print("3. To simulate games (s)")
 print()
+print("Use \'clear\' to clean the terminal")
 print("To print help press (h)")
+print("To quit press (q)")
+
 while(loop):
     command = input(
         "> ")
@@ -22,12 +37,41 @@ while(loop):
     if command == "q":
         loop = False
 
-    if command == "h":
+    elif command == "TTT":
+        tic.play("nn", "p")
+
+    elif command == "C4":
+        c4.play("nn", "p")
+
+    elif command == "s":
+        ut.clear()
+        print("Simulating Games...")
+        print()
+        print("Making our Tic Tac Toe neural network play")
+        print("against a random player a 1000 times!")
+        print()
+        print("This will take a couple of seconds...")
+        print()
+        tic.simulate_games(1000, "nn", "r")
+        print()
+        input("[Press Enter to continue]")
+        print()
+        print("Making our Connect Four neural network play")
+        print("against a random player a 1000 times!")
+        print()
+        print("This will take a couple of seconds...")
+        print()
+        c4.simulate_games(1000, "nn", "r")
+
+    elif command == "h":
         print()
         print("1. To play Tic Tac Toe (TTT)")
         print("2. To play Connect Four (C4)")
+        print("3. To simulate games (s)")
         print()
+        print("Use \'clear\' to clean the terminal")
         print("To print help press (h)")
+        print("To quit press (q)")
 
-    if command == "clear":
+    elif command == "clear":
         ut.clear()
