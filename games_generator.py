ad9bd3bd0cb3f0c194c4395afc4e795e8cde5fbc
Afonso <afonsorafael@sapo.pt>
2021-01-18 00:26:04 +0000

    comments and edits
    changed the games_generator file as well.
    includes now proper documentation.

--- games_generator.py
+++ games_generator.py
@@ -1,12 +1,25 @@
-# Tic tac toe game generator
+# Tic tac toe
 #
-# @Author: Afonso Rafael
+# @Author: Afonso Rafael & Renata
+#
+# Game generator. Functions related to
+# generating various amounts of games to
+# train the neural networks.
 
 from itertools import chain
+
+# Chain is used to produce the strings
+# that are saved in the file
 
 
 def gen_games(game, n_games, path=None,
               file_name="games"):
+    """
+    Generates files of games of any kind. What
+    writes onto the files is the final result
+    that is given by the play() function, and
+    the history that comes with it.
+    """
 
     if path is None:
         file_ = open(file_name, "w")
@@ -14,7 +27,8 @@ def gen_games(game, n_games, path=None,
         file_ = open(str(path) + file_name, "w")
 
     while(n_games > 0):
-        file_.write(str(list(chain(*game.play(player1_mode='r', player2_mode='r')))))
+        file_.write(str(list(chain(
+            *game.play(player1_mode='r', player2_mode='r')))))
         file_.write("\n")
         n_games -= 1
     return 0
