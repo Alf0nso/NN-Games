b51b582d93717254946406a5a96fe730fc8d7110
Afonso <afonsorafael@sapo.pt>
2021-01-18 02:58:49 +0000

    Prompt coded on main
    Coded a prompt on main. What I have in vision
    is to make the main file run a prompt where
    you are able to view in an easy a way a showcase
    of our project!

--- main.py
+++ main.py
@@ -4,8 +4,30 @@ import connect4 as c4
 import tic_tac_toe as tic
 import games_generator as gg
 
-# generate games
-# gg.gen_games(tic, 1000, file_name='tictic')
-# gg.gen_games(c4, 10000, file_name='connect4_games')
+loop = True
 
-tic.play("nn", "p")
+ut.clear()
+print(" " + 40*"-")
+print("    Neural Networks On Strategy Games!")
+print(" " + 40*"-")
+print("\n\n")
+print("1. To play Tic Tac Toe (TTT)")
+print("2. To play Connect Four (C4)")
+print()
+print("To print help press (h)")
+while(loop):
+    command = input(
+        "> ")
+
+    if command == "q":
+        loop = False
+
+    if command == "h":
+        print()
+        print("1. To play Tic Tac Toe (TTT)")
+        print("2. To play Connect Four (C4)")
+        print()
+        print("To print help press (h)")
+
+    if command == "clear":
+        ut.clear()
